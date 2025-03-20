d3277c3dd47ad6a53199f164ad6ce640b3c2a253
DWSuryo <72719372+DWSuryo@users.noreply.github.com>
2025-03-20 08:42:59 +0700

    add duration

--- main_edit.py
+++ main_edit.py
@@ -1,8 +1,11 @@
 from scripts import train, ava_eval, ucf_eval, detect, live
 import argparse
 from utils.build_config import build_config
+from datetime import datetime
 
 if __name__ == "__main__":
+    time_start = datetime.now()
+    print("Started at Date and Time:", time_start.strftime("%Y-%m-%d %H:%M:%S"))
     parser = argparse.ArgumentParser(description="YOWOv3")
 
     parser.add_argument('-m', '--mode', type=str, help='train/eval/live/detect/onnx')
@@ -29,3 +32,15 @@ if __name__ == "__main__":
     
     elif args.mode == 'onnx':
         onnx.export2onnx(config=config)
+
+    time_end = datetime.now()
+    print("Finished at Date and Time:", time_end.strftime("%Y-%m-%d %H:%M:%S"))
+    time_duration = time_end - time_start
+    # Format the duration as Days HH:MM:SS
+    days = time_duration.days
+    seconds = time_duration.seconds
+    hours, remainder = divmod(seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+
+    formatted_duration = f"{days} Days {hours:02}:{minutes:02}:{seconds:02}"
+    print(f"Code execution time: {formatted_duration}")
